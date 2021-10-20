6c5089e0f4e19e6e3310a052647bfe0e580299f6
Bhogeswararao <92287678+Bhogeswararao@users.noreply.github.com>
2021-10-20 10:49:11 +0530

    Add files via upload

---
+++ char,int,float.c
@@ -0,0 +1,18 @@
+//adding of numbers
+#include<stdio.h>
+int main()
+{
+	//decleration
+	char name;
+	int age;
+	float cgpa;
+	//initialization
+	name='B';
+	age=20;
+	cgpa=9.3;
+	//output 
+	printf("%c\n",name);
+	printf("%d\n",age);
+	printf("%f",cgpa);
+}
+
