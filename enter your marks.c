d6a05e3907f3cb7e1e30d78b58c100a3c362f48a
Bhogeswararao <92287678+Bhogeswararao@users.noreply.github.com>
2021-10-25 10:21:36 +0530

    Add files via upload

---
+++ enter your marks.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int phy,eng,math,che,tot;
+	scanf("%d %d %d %d",&phy,&eng,&math,&che);
+	tot=phy+eng+math+che;
+	printf("marks in physics: %d\n",phy);
+	printf("marks in english: %d\n",eng);
+	printf("marks in maths: %d\n",math);
+	printf("marks in chemistry: %d\n",che);
+	printf("total marks: %d",tot);
+	
+	
+}
---
+++ marks.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int phy,eng,math,che,tot;
+	scanf("%d %d %d %d %d",&phy&eng&math&che&tot);
+	tot=phy+eng+math+che;
+	printf("marks in physics: %d\n",phy);
+	printf("marks in english: %d\n",eng);
+	printf("marks in maths: %d\n",math);
+	printf("marks in chemistry: %d\n",che);
+	printf("marks in physics: %d\n",phy);
+	printf("total marks: %d",tot);
+	
+	
+}
