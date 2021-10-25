990374b767dd322f37b9cf2d8e23f5bcabf24939
Bhogeswararao <92287678+Bhogeswararao@users.noreply.github.com>
2021-10-25 10:23:15 +0530

    Update marks.c

--- marks.c
+++ marks.c
@@ -2,13 +2,12 @@
 int main()
 {
 	int phy,eng,math,che,tot;
-	scanf("%d %d %d %d %d",&phy&eng&math&che&tot);
+	scanf("%d %d %d %d ",&phy,&eng,&math,&che);
 	tot=phy+eng+math+che;
 	printf("marks in physics: %d\n",phy);
 	printf("marks in english: %d\n",eng);
 	printf("marks in maths: %d\n",math);
 	printf("marks in chemistry: %d\n",che);
-	printf("marks in physics: %d\n",phy);
 	printf("total marks: %d",tot);
 	
 	
